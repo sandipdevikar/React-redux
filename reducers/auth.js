ebc92becbc6606e9b8ce4b372686b9c2a335367b
sandipdevikar <sandipdevikar007@gmail.com>
2020-01-06 13:22:51 +0530

    2 files modified

--- reducers/auth.js
+++ reducers/auth.js
@@ -1,13 +1,19 @@
 const initialState ={
   name:"sandip ramdas Devikar",
   email:"sandipdevikar007@gmail.com",
-  password:"asdfssf"
+  password:"asdfssf",
+  user:null
 
 }
 
 export default function(state=initialState,action){
   switch(action.type){
 
+    case "ADD_USER":
+    return{
+      ...state,
+  user:action.payload
+    }
     default:
     return state;
 
